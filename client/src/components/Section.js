00c0222074e7d27c5cc7b071ee11ae3e74efdcc2
Mert <mertuygur02@gmail.com>
2021-04-20 17:43:09 +0300

    sections on home page designed, looks nice.

--- client/src/components/Section.js
+++ client/src/components/Section.js
@@ -4,18 +4,10 @@ const Section = () => {
     return (
         <div className="section">
             <div className="section__pic">
-                <img src="/images/sec.svg" width="50px" alt="sec pic" />
+                <img src="/images/phil.svg" width="650px" alt="sec pic" />
             </div>
             <div className="section__text">
-                <div className="section__text__title">
                     <h3>Stoicism</h3>
-                </div>
-                <div className="section__text__def">
-                    <p>Posts and discussions about stoicisim and related fields.</p>
-                </div>
-                <div className="section__text__info">
-                    <p>Last Activity : 8 hours ago</p>
-                </div>
             </div>
         </div>
     )
